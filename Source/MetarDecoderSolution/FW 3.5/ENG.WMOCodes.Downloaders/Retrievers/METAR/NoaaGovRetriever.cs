af35f7df70838ce9be6df9c2524a12c562c34914
fedarovich <p.fedarovich@gmail.com>
2015-09-03 00:07:28 +0300

    Added Downloader.

--- Source/MetarDecoderSolution/FW 3.5/ENG.WMOCodes.Downloaders/Retrievers/METAR/NoaaGovRetriever.cs
+++ Source/MetarDecoderSolution/FW 3.5/ENG.WMOCodes.Downloaders/Retrievers/METAR/NoaaGovRetriever.cs
@@ -1,44 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+#if PCL
+using System.Threading.Tasks;
+#endif
 
 namespace ENG.WMOCodes.Downloaders.Retrievers.Metar
 {
-  /// <summary>
-  /// Downloads metar from web noaa.gov.
-  /// </summary>
-  /// <seealso cref="T:ENG.Metar.Downloader.IMetarRetrieve"/>
-  public class NoaaGovRetriever : IRetriever
-  {
-    #region IMetarRetrieve Members
-
     /// <summary>
-    /// Returns URL where METAR information is stored.
+    /// Downloads metar from web noaa.gov.
     /// </summary>
-    /// <param name="icao">ICAO code of airport/station.</param>
-    /// <returns></returns>
-    public string GetUrlForICAO(string icao)
+    /// <seealso cref="T:ENG.Metar.Downloader.IMetarRetrieve"/>
+    public class NoaaGovRetriever : IRetriever
     {
-      return "http://weather.noaa.gov/pub/data/observations/metar/stations/" + icao.ToUpper() + ".TXT";
+        #region IMetarRetrieve Members
+
+        /// <summary>
+        /// Returns URL where METAR information is stored.
+        /// </summary>
+        /// <param name="icao">ICAO code of airport/station.</param>
+        /// <returns></returns>
+        public string GetUrlForICAO(string icao)
+        {
+            return "http://weather.noaa.gov/pub/data/observations/metar/stations/" + icao.ToUpper() + ".TXT";
+        }
+
+#if PCL
+
+        /// <summary>
+        /// Decodes code as string from stream asynchronously. 
+        /// Stream should be downloaded from URL address obtained 
+        /// from <see cref="GetUrlForICAO"/> method.
+        /// </summary>
+        /// <param name="sourceStream">Source stream, from which the metar will be obtained.</param>
+        /// <returns>Code string.</returns>
+        /// <exception cref="DownloadException">
+        /// Returns if anything fails. Inner exception should contain more accurate info.
+        /// </exception>
+        public async Task<string> DecodeWMOCodeAsync(Stream sourceStream)
+        {
+            var rdr = new StreamReader(sourceStream);
+            await rdr.ReadLineAsync().ConfigureAwait(false);
+            string r = await rdr.ReadLineAsync().ConfigureAwait(false);
+
+            return "METAR " + r;
+        }
+
+#else
+
+        /// <summary>
+        /// Decodes metar from stream. Stream should be downloaded from URL address obtained 
+        /// from GetUrlForICAO() method. <seealso cref="GetUrlForICAO"/>.
+        /// </summary>
+        /// <param name="sourceStream">Source stream, from which the metar will be obtained.</param>
+        /// <returns>Metar string.</returns>
+        /// <exception cref="DownloadException">Returns if anything fails. Inner exception should contain more accurate info.</exception>
+        public string DecodeWMOCode(System.IO.Stream sourceStream)
+        {
+            System.IO.StreamReader rdr = new System.IO.StreamReader(sourceStream);
+            rdr.ReadLine();
+            string r = rdr.ReadLine();
+
+            return "METAR " + r;
+        }
+
+#endif
+
+        #endregion
     }
-
-    /// <summary>
-    /// Decodes metar from stream. Stream should be downloaded from URL address obtained 
-    /// from GetUrlForICAO() method. <seealso cref="GetUrlForICAO"/>.
-    /// </summary>
-    /// <param name="sourceStream">Source stream, from which the metar will be obtained.</param>
-    /// <returns>Metar string.</returns>
-    /// <exception cref="DownloadException">Returns if anything fails. Inner exception should contain more accurate info.</exception>
-    public string DecodeWMOCode(System.IO.Stream sourceStream)
-    {
-      System.IO.StreamReader rdr = new System.IO.StreamReader(sourceStream);
-      rdr.ReadLine();
-      string r = rdr.ReadLine();
-
-      return "METAR " + r;
-    }
-
-    #endregion
-  }
 }
--- Source/MetarDecoderSolution/Portable/ENG.WMOCodes.Downloaders/Downloader.cs
+++ Source/MetarDecoderSolution/Portable/ENG.WMOCodes.Downloaders/Downloader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ENG.WMOCodes.Downloaders
 {
@@ -10,6 +9,108 @@ namespace ENG.WMOCodes.Downloaders
     /// </summary>
     public class Downloader
     {
-        // TODO: implement
+        private readonly IRetriever retriever;
+
+        /// <summary>
+        /// Delegate used to announce when asynchronous download is completed.
+        /// Is used for both, successful and unsuccessful downloads.
+        /// </summary>
+        /// <param name="result">Result containing data</param>
+        public delegate void DownloadCompletedDelegate(RetrieveResult result);
+
+        public Downloader(IRetriever retriever)
+        {
+            if (retriever == null)
+                throw new ArgumentNullException("retriever");
+
+            this.retriever = retriever;
+        }
+
+        /// <summary>
+        /// Download metar asynchronously.
+        /// </summary>
+        /// <param name="icao">Icao code of airport/station.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="icao"/> is <see langword="null"/>.</exception>
+        public Task<string> DownloadAsync(string icao)
+        {
+            return DownloadAsync(icao, new HttpClientHandler(), true);
+        }
+
+        /// <summary>
+        /// Download metar asynchronously.
+        /// </summary>
+        /// <param name="icao">Icao code of airport/station.</param>
+        /// <param name="handler">The HTTP handler stack to use for sending requests.</param>
+        /// <param name="disposeHandler">The value indicating whether the <paramref name="handler"/> must be disposed after the operation completes.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="icao"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentNullException"><paramref name="handler"/> is <see langword="null"/>.</exception>
+        public async Task<string> DownloadAsync(
+            string icao, 
+            HttpMessageHandler handler, 
+            bool disposeHandler)
+        {
+            if (icao == null)
+                throw new ArgumentNullException("icao");
+            if (handler == null)
+                throw new ArgumentNullException("handler");
+
+            var url = retriever.GetUrlForICAO(icao);
+            var httpClient = new HttpClient(handler, disposeHandler);
+            try
+            {
+                using (var response = await httpClient.GetStreamAsync(url).ConfigureAwait(false))
+                {
+                    return await retriever.DecodeWMOCodeAsync(response).ConfigureAwait(false);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new DownloadException("Failed to download metar from web.", ex);
+            }
+            finally
+            {
+                httpClient.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// Download metar asynchronously.
+        /// </summary>
+        /// <param name="icao">Icao code of airport/station.</param>
+        /// <param name="downloadCompletedDelegate">Delegate function raised when download is completed or error occured.</param>
+        /// <exception cref="DownloadException">
+        /// Raised when any error occurs.
+        /// </exception>
+        [Obsolete("Use the overload returning Task<string> instead.")]
+        public async void DownloadAsync(string icao, DownloadCompletedDelegate downloadCompletedDelegate)
+        {
+            try
+            {
+                var metar = await DownloadAsync(icao);
+                downloadCompletedDelegate(new RetrieveResult(metar));
+            }
+            catch (Exception ex)
+            {
+                downloadCompletedDelegate(new RetrieveResult(ex));
+            }
+        }
+
+        /// <summary>
+        /// Download metar asynchronously.
+        /// </summary>
+        /// <param name="icao">Icao code of airport/station.</param>
+        /// <param name="retriever">Metar retrievere used to decode metar from source stream</param>
+        /// <param name="downloadCompletedDelegate">Delegate function raised when download is completed or error occured.</param>
+        /// <exception cref="DownloadException">
+        /// Raised when any error occurs.
+        /// </exception>
+        [Obsolete("Use the overload returning Task<string> instead.")]
+        public static void DownloadAsync(string icao, IRetriever retriever,
+            DownloadCompletedDelegate downloadCompletedDelegate)
+        {
+#pragma warning disable 618
+            new Downloader(retriever).DownloadAsync(icao, downloadCompletedDelegate);
+#pragma warning restore 618
+        }
     }
 }

e81514e9b07946cae2fdccd22b7cfa820a09858e
fedarovich <p.fedarovich@gmail.com>
2015-09-02 23:12:41 +0300

    Added portable class libraries Note: Downloader is missing.

---
+++ Source/MetarDecoderSolution/Portable/ENG.WMOCodes/Extensions/ComparableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENG.WMOCodes.Extensions
+{
+    internal static class ComparableExtensions
+    {
+        public static bool IsBetween<T>(this IComparable<T> value, T firstBound, T secondBound) where T : IComparable<T>
+        {
+            if (firstBound.CompareTo(secondBound) > 0)
+            {
+                T obj = firstBound;
+                firstBound = secondBound;
+                secondBound = obj;
+            }
+            return value.CompareTo(firstBound) >= 0 && value.CompareTo(secondBound) <= 0;
+        }
+    }
+}
---
+++ Source/MetarDecoderSolution/Portable/ENG.WMOCodes/Extensions/ObjectExtensions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace ENG.WMOCodes.Extensions
+{
+    internal static class ObjectExtensions
+    {
+        private static int callIndex = 0;
+        private const string NULL = "(null)";
+        private const string INTENDER = "  ";
+        private const string DEFAULT_ENCAPSULATING_TAG = "This";
+        private const string DEFAULT_ENUMERATIONITEM_TAG = "Item";
+        private const string DEFAULT_NULL_TAGITEM = "(null)";
+
+        public static string ToInlineInfoString(this object obj)
+        {
+            ++callIndex;
+            if (obj == null)
+                return "(null)";
+            if (obj is string)
+                return Convert.ToString(obj);
+            StringBuilder builder = new StringBuilder();
+            StringBuilder stringBuilder = new StringBuilder();
+            Type type = obj.GetType();
+            if (obj is ICollection)
+            {
+                foreach (object item in (IEnumerable)obj)
+                {
+                    string text = ToInlineInfoString(item);
+                    builder.AppendPreDelimited(text, ';');
+                }
+            }
+            else if (obj is IEnumerable)
+            {
+                foreach (object item in (IEnumerable)obj)
+                {
+                    string text = ToInlineInfoString(item);
+                    builder.AppendPreDelimited(text, ';');
+                }
+            }
+            else
+            {
+                var properties = type.GetProperties();
+                string str = GenerateInlineContent(obj, properties);
+                if (str.Length > 0)
+                {
+                    builder.AppendPreDelimited(str, ';');
+                }
+            }
+            if (builder.Length == 0 && !(obj is ICollection))
+            {
+                stringBuilder.Append(obj);
+            }
+            else
+            {
+                stringBuilder.Append("{" + builder + "}");
+                stringBuilder.Insert(0, "(" + type.Name + ")");
+            }
+            return stringBuilder.ToString();
+        }
+
+        private static string GenerateInlineContent(object obj, PropertyInfo[] properties)
+        {
+            StringBuilder builder = new StringBuilder();
+            foreach (var propertyInfo in properties)
+            {
+                string str = ToInlineInfoString(propertyInfo.GetValue(obj, null));
+                builder.AppendPreDelimited(propertyInfo.Name + "=" + str, ',');
+            }
+            return builder.ToString();
+        }
+    }
+}

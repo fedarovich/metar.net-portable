0a9eb37e92187125309de94b091057198dc4a6da
Pavel <p.fedarovich@gmail.com>
2017-03-13 01:56:53 +0300

    Migrated to VS2017

---
+++ Source/MetarDecoderSolution/ENG.WMOCodes.Downloaders/DownloadException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENG.WMOCodes.Downloaders
+{
+    /// <summary>
+    /// Raised when some error occurs during metar downloading or decoding.
+    /// Inner exception should contain more accurate information.
+    /// </summary>
+    public class DownloadException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DownloadException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        public DownloadException(string message) : base(message) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DownloadException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner.</param>
+        public DownloadException(string message, Exception inner) : base(message, inner) { }
+    }
+}

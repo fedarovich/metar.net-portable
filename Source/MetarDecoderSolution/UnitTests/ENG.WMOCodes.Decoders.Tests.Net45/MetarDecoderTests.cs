bb3cabe751e3294c76802ceef95158d45f8d5161
Pavel <p.fedarovich@gmail.com>
2017-03-18 11:58:09 +0300

    Update packages. Added unit test to reproduce Issue #1

---
+++ Source/MetarDecoderSolution/UnitTests/ENG.WMOCodes.Decoders.Tests.Net45/MetarDecoderTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ENG.WMOCodes.Decoders.Tests.Net45
+{
+    public class MetarDecoderTests
+    {
+        [Fact]
+        public void Bug1()
+        {
+            var decoder = new MetarDecoder();
+            var report = decoder.Decode(
+                "METAR KEWR 142108Z 30017G26KT 1SM R04R/5000VP6000FT -SN BLSN SCT009 OVC017 M01/M03 A2926 RMK AO2 PK WND 31031/2053 SFC VIS 1 1/2 P0000 T10111033");
+        }
+    }
+}
